60b08371675c758f6b5a7c99348f7b3ea0d8a0d9
TeleGold <omid.http@gmail.com>
2016-06-24 20:59:50 +0430

    Update infull.lua

--- plugins/infull.lua
+++ plugins/infull.lua
@@ -1,132 +1,127 @@
 do
-local gorg sefat = 67559249 --put your id here(BOT OWNER ID)
+local Arian = 194849320 --put your id here(BOT OWNER ID)
 
 local function setrank(msg, name, value) -- setrank function
   local hash = nil
-   if msg.to.type == 'chat' or msg.to.type == 'channel'  then
+  if msg.to.type == 'chat' then
     hash = 'rank:'..msg.to.id..':variables'
   end
   if hash then
     redis:hset(hash, name, value)
-	return send_msg('chat#id'..msg.to.id, 'set Rank for ('..name..') To : '..value, ok_cb,  true)
+	return send_msg('chat#id'..msg.to.id, 'مقام کاربر ('..name..') به\n '..value..' تغییر داده شد. ', ok_cb,  true)
   end
 end
-
-
-local function res_user_callback(extra, success, result) -- /infull <username> function
+local function res_user_callback(extra, success, result) -- /info <username> function
   if success == 1 then  
   if result.username then
    Username = '@'..result.username
    else
-   Username = '----'
+   Username = 'ندارد'
   end
-    local text = 'Full name : '..(result.first_name or '')..' '..(result.last_name or '')..'\n'
-               ..'User name: '..Username..'\n'
-               ..'ID : '..result.id..'\n\n'
-               .."phone number : +"..(msg.from.phone or '----')..'\n'
+    local text = '🔱› نام کاربر : '..(result.first_name or '')..' '..(result.last_name or '')..'\n'
+               ..'🔱› نام کاربری : '..Username..'\n'
+               ..'🔱› کد کاربری : '..result.id..'\n'
 	local hash = 'rank:'..extra.chat2..':variables'
 	local value = redis:hget(hash, result.id)
     if not value then
 	 if result.id == tonumber(Arian) then
-	   text = text..'Rank : Executive Admin \n\n'
+	   text = text..'🔱› مقام : مدیر کل ربات (Sudo) \n'
 	  elseif is_admin2(result.id) then
-	   text = text..'Rank : Admin \n\n'
+	   text = text..'🔱› مقام : ادمین ربات (Admin) \n'
 	  elseif is_owner2(result.id, extra.chat2) then
-	   text = text..'Rank : Owner \n\n'
+	   text = text..'🔱› مقام : مدیر کل گروه (Owner) \n'
 	  elseif is_momod2(result.id, extra.chat2) then
-	    text = text..'Rank : Moderator \n\n'
+	    text = text..'🔱› مقام : مدیر گروه (Moderator) \n'
       else
-	    text = text..'Rank : Member \n\n'
+	    text = text..'🔱› مقام : کاربر (Member) \n'
 	 end
    else
-   text = text..'Rank : '..value..'\n\n'
+   text = text..'🔱› مقام : '..value..'\n'
   end
   local uhash = 'user:'..result.id
   local user = redis:hgetall(uhash)
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
-  user_infull_msgs = tonumber(redis:get(um_hash) or 0)
-  text = text..'Total messages : '..user_infull_msgs..'\n\n'
-  text = text..'@WaderTGTeam'
+  user_info_msgs = tonumber(redis:get(um_hash) or 0)
+  text = text..'🔱› تعداد پیام های فرستاده شده : '..user_info_msgs..'\n'
+  text = text..'ِ'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
-	send_msg(extra.receiver, ' Username not found.', ok_cb, false)
+	send_msg(extra.receiver, extra.user..'🔱 نام کاربری مورد نظر یافت نشد.🔱', ok_cb, false)
   end
 end
 
-local function action_by_id(extra, success, result)  -- /infull <ID> function
+local function action_by_id(extra, success, result)  -- /info <ID> function
  if success == 1 then
  if result.username then
    Username = '@'..result.username
    else
-   Username = '----'
+   Username = 'ندارد'
  end
-   local text = 'Full name : '..(result.first_name or '')..' '..(result.last_name or '')..'\n'
-               ..'Username: '..Username..'\n'
-               ..'ID : '..result.id..'\n\n'
-               .."phone number : +"..(msg.from.phone or '----')..'\n'
+    local text = '🔱› نام کاربر : '..(result.first_name or '')..' '..(result.last_name or '')..'\n'
+               ..'🔱› نام کاربری : '..Username..'\n'
+               ..'🔱› کد کاربری : '..result.id..'\n'
   local hash = 'rank:'..extra.chat2..':variables'
   local value = redis:hget(hash, result.id)
   if not value then
 	 if result.id == tonumber(Arian) then
-	   text = text..'Rank : Executive Admin \n\n'
+	   text = text..'🔱› مقام : مدیر کل ربات (Sudo) \n'
 	  elseif is_admin2(result.id) then
-	   text = text..'Rank : Admin \n\n'
+	   text = text..'🔱› مقام : ادمین ربات (Admin) \n'
 	  elseif is_owner2(result.id, extra.chat2) then
-	   text = text..'Rank : Owner \n\n'
+	   text = text..'🔱› مقام : مدیر کل گروه (Owner) \n'
 	  elseif is_momod2(result.id, extra.chat2) then
-	   text = text..'Rank : Moderator \n\n'
+	   text = text..'🔱› مقام : مدیر گروه (Moderator) \n'
 	  else
-	   text = text..'Rank : Member \n\n'
+	   text = text..'🔱› مقام : کاربر (Member) \n'
 	  end
    else
-    text = text..'Rank : '..value..'\n\n'
+    text = text..'🔱› مقام : '..value..'\n'
   end
   local uhash = 'user:'..result.id
   local user = redis:hgetall(uhash)
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
-  user_infull_msgs = tonumber(redis:get(um_hash) or 0)
-  text = text..'Total messages : '..user_infull_msgs..'\n\n'
-  text = text..'@WaderTGTeam'
+  user_info_msgs = tonumber(redis:get(um_hash) or 0)
+  text = text..'🔱› تعداد پیام های فرستاده شده : '..user_info_msgs..'\n'
+  text = text..'ِ'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
-  send_msg(extra.receiver, 'id not found.\nuse : /infull @username', ok_cb, false)
+  send_msg(extra.receiver, '🔱آیدی شخص مورد نظر در سیستم ثبت نشده است.\nاز دستور زیر استفاده کنید\n/info @username', ok_cb, false)
   end
 end
 
-local function action_by_reply(extra, success, result)-- (reply) /infull  function
+local function action_by_reply(extra, success, result)-- (reply) /info  function
 		if result.from.username then
 		   Username = '@'..result.from.username
 		   else
-		   Username = '----'
+		   Username = 'ندارد'
 		 end
-  local text = 'Full name : '..(result.from.first_name or '')..' '..(result.from.last_name or '')..'\n'
-               ..'Username : '..Username..'\n'
-               ..'ID : '..result.from.id..'\n\n'
-	           .."phone number : +"..(msg.from.phone or '----')..'\n'
+    local text = '🔱› نام کاربر : '..(result.from.first_name or '')..' '..(result.from.last_name or '')..'\n'
+               ..'🔱› نام کاربری : '..Username..'\n'
+               ..'🔱› کد کاربری : '..result.from.id..'\n'
 	local hash = 'rank:'..result.to.id..':variables'
 		local value = redis:hget(hash, result.from.id)
 		 if not value then
 		    if result.from.id == tonumber(Arian) then
-		       text = text..'Rank :Executive Admin \n\n'
+		       text = text..'🔱› مقام : مدیر کل ربات (Sudo) \n'
 		     elseif is_admin2(result.from.id) then
-		       text = text..'Rank : Admin \n\n'
+		       text = text..'🔱› مقام : ادمین ربات (Admin) \n'
 		     elseif is_owner2(result.from.id, result.to.id) then
-		       text = text..'Rank : Owner \n\n'
+		       text = text..'🔱› مقام : مدیر کل گروه (Owner) \n'
 		     elseif is_momod2(result.from.id, result.to.id) then
-		       text = text..'Rank : Moderator \n\n'
+		       text = text..'🔱› مقام : مدیر گروه (Moderator) \n'
 		 else
-		       text = text..'Rank : Member \n\n'
+		       text = text..'🔱› مقام : کاربر (Member) \n'
 			end
 		  else
-		   text = text..'Rank : '..value..'\n\n'
+		   text = text..'🔱› مقام : '..value..'\n'
 		 end
-         local user_infull = {} 
+         local user_info = {}
   local uhash = 'user:'..result.from.id
   local user = redis:hgetall(uhash)
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
-  user_infull_msgs = tonumber(redis:get(um_hash) or 0)
-  text = text..'Total messages : '..user_infull_msgs..'\n\n'
-  text = text..'@WaderTGTeam'
+  user_info_msgs = tonumber(redis:get(um_hash) or 0)
+  text = text..'🔱› تعداد پیام های فرستاده شده : '..user_info_msgs..'\n'
+  text = text..'ِ'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
@@ -140,7 +135,7 @@ local function run(msg, matches)
   local hash = 'usecommands:'..msg.from.id..':'..msg.to.id
   redis:incr(hash)
   if not is_sudo(msg) then
-    return "konde mage to babami?"
+    return "🔱فقط سازنده میتواند.🔱"
   end
   local receiver = get_receiver(msg)
   local Reply = msg.reply_id
@@ -155,7 +150,7 @@ local function run(msg, matches)
   return text
   end
   end
- if matches[1]:lower() == 'infull' and not matches[2] then
+ if matches[1]:lower() == 'info' or matches[1]:lower() == 'id' or matches[1]:lower() == 'me' and not matches[2] then
   local receiver = get_receiver(msg)
   local Reply = msg.reply_id
   if msg.reply_id then
@@ -164,51 +159,51 @@ local function run(msg, matches)
   if msg.from.username then
    Username = '@'..msg.from.username
    else
-   Username = '----'
+   Username = 'ندارد'
    end
-   local text = 'First name : '..(msg.from.first_name or '----')..'\n'
-   local text = text..'Last name : '..(msg.from.last_name or '----')..'\n'	
-   local text = text..'phone number : +'..(msg.from.phone or '----')..'\n'
-   local text = text..'Username : '..Username..'\n'
-   local text = text..'ID : '..msg.from.id..'\n\n'
+   local text = '🔱› نام : '..(msg.from.first_name or 'ندارد')..'\n'
+   local text = text..'🔱› فامیل : '..(msg.from.last_name or 'ندارد')..'\n'	
+   local text = text..'🔱› نام کاربری : '..Username..'\n'
+   local text = text..'🔱› کد کاربری : '..msg.from.id..'\n'
    local hash = 'rank:'..msg.to.id..':variables'
 	if hash then
 	  local value = redis:hget(hash, msg.from.id)
 	  if not value then
 		if msg.from.id == tonumber(Arian) then
-		 text = text..'Rank : Executive Admin \n\n'
+		 text = text..'🔱› مقام : مدیر کل ربات (Sudo) \n'
 		elseif is_sudo(msg) then
-		 text = text..'Rank : Admin \n\n'
+		 text = text..'🔱› مقام : ادمین ربات (Admin) \n'
 		elseif is_owner(msg) then
-		 text = text..'Rank : Owner \n\n'
+		 text = text..'🔱› مقام : مدیر کل گروه (Owner) \n'
 		elseif is_momod(msg) then
-		 text = text..'Rank :Moderator \n\n'
+		 text = text..'🔱› مقام : مدیر گروه (Moderator) \n'
 		else
-		 text = text..'Rank : Member \n\n'
+		 text = text..'🔱› مقام : کاربر (Member) \n'
 		end
 	  else
-	   text = text..'Rank : '..value..'\n'
+	   text = text..'🔱› مقام : '..value..'\n'
 	  end
 	end
+    
 	 local uhash = 'user:'..msg.from.id
  	 local user = redis:hgetall(uhash)
   	 local um_hash = 'msgs:'..msg.from.id..':'..msg.to.id
-	 user_infull_msgs = tonumber(redis:get(um_hash) or 0)
-	 text = text..'Total messages : '..user_infull_msgs..'\n\n'
-    if msg.to.type == 'chat' then
-	 text = text..'Group name : '..msg.to.title..'\n'
-     text = text..'Group ID : '..msg.to.id
+	 user_info_msgs = tonumber(redis:get(um_hash) or 0)
+	 text = text..'🔱› تعداد پیام های فرستاده شده : '..user_info_msgs..'\n'
+	 if msg.to.type == 'chat' then
+	 text = text..'🔱› نام گروه : '..msg.to.title..'\n'
+     text = text..'🔱› کد گروه : '..msg.to.id
     end
-	text = text..'\n\n@WaderTGTeam'
+	text = text..'\nِ'
     return send_msg(receiver, text, ok_cb, true)
     end
   end
-  if matches[1]:lower() == 'infull' and matches[2]  then
+  if matches[1]:lower() == 'info' and matches[2] then
    local user = matches[2]
    local chat2 = msg.to.id
    local receiver = get_receiver(msg)
    if string.match(user, '^%d+$') then
-	  user_infull('user#id'..user, action_by_id, {receiver=receiver, user=user, text=text, chat2=chat2})
+	  user_info('user#id'..user, action_by_id, {receiver=receiver, user=user, text=text, chat2=chat2})
     elseif string.match(user, '^@.+$') then
       username = string.gsub(user, '@', '')
       msgr = res_user(username, res_user_callback, {receiver=receiver, user=user, text=text, chat2=chat2})
@@ -217,20 +212,27 @@ local function run(msg, matches)
 end
 
 return {
-  description = 'Know your infullrmation or the info of a chat members.',
+  description = 'Know your information or the info of a chat members.',
   usage = {
-	'!infull: Return your info and the chat info if you are in one.',
+	'!info: Return your info and the chat info if you are in one.',
 	'(Reply)!info: Return info of replied user if used by reply.',
-	'!infull <id>: Return the info\'s of the <id>.',
-	'!infull @<user_name>: Return the member @<user_name> information from the current chat.',
+	'!info <id>: Return the info\'s of the <id>.',
+	'!info @<user_name>: Return the member @<user_name> information from the current chat.',
 	'!setrank <userid> <rank>: change members rank.',
 	'(Reply)!setrank <rank>: change members rank.',
   },
   patterns = {
-	"^[#!/](infull)$",
-	"^[#!/](infull)(.*)$",
-        "^([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (%d+) (.*)$",
-	"^([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (.*)$"  },
+	"^[/#!]([Ii][Nn][Ff][Oo])$",
+	"^[!#/]([Ii][Dd])$",
+	"^[#!/]([Mm][Ee])$",
+	"^[/#!]([Ii][Nn][Ff][Oo]) (.*)$",
+	"^[/#!]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (%d+) (.*)$",
+	"^[/#!]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (.*)$",
+	"^([Ii][Nn][Ff][Oo])$",
+	"^([Ii][Nn][Ff][Oo]) (.*)$",
+	"^([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (%d+) (.*)$",
+	"^([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (.*)$",
+  },
   run = run
 }
 
