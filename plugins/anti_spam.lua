844a9e6a306e26af82390cbeeab74fb9b1aa7eaa
TeleGold <omid.http@gmail.com>
2016-06-24 16:59:46 +0430

    Update anti_spam.lua

--- plugins/anti_spam.lua
+++ plugins/anti_spam.lua
@@ -1,9 +1,8 @@
---An empty table for solving multiple kicking problem(thanks to @topkecleon )
 kicktable = {}
 
 do
 
-local TIME_CHECK = 2 -- seconds
+local TIME_CHECK = 3 -- seconds
 -- Save stats, ban user
 local function pre_process(msg)
   -- Ignore service msg
@@ -92,7 +91,7 @@ local function pre_process(msg)
 		print(msgs)
 		if msgs >= max_msg then
 			print("Pass2")
-			send_large_msg("user#id"..msg.from.id, "User ["..msg.from.id.."] blocked for spam.")
+			send_large_msg("user#id"..msg.from.id, "🔱 کاربر: ["..msg.from.id.."] بدلیل اسپم های مکرر و تکراری از ربات تله گولد بلاک شد! 🔱 .")
 			savelog(msg.from.id.." PM", "User ["..msg.from.id.."] blocked for spam.")
 			block_user("user#id"..msg.from.id,ok_cb,false)--Block user if spammed in private
 		end
@@ -107,11 +106,11 @@ local function pre_process(msg)
 	  local name_log = print_name:gsub("_", "")
 	  if msg.to.type == 'chat' or msg.to.type == 'channel' then
 		if username then
-			savelog(msg.to.id, name_log.." @"..username.." ["..msg.from.id.."] kicked for #spam")
-			send_large_msg(receiver , "Flooding is not allowed here\n@"..username.."["..msg.from.id.."]\nStatus: User kicked")
+			savelog(msg.to.id, name_log.."🔱 @"..username.." ["..msg.from.id.."] از گروه خارج شد برای استفاده از اسپم های مکرر.")
+			send_large_msg(receiver , "🔱 متاسفانه اسپم زدن در این گروه مجاز نمیباشد! \n🔱 مشخصات کاربر:\n🔱 یوزر: @"..username.."\n🔱 آیدی: ["..msg.from.id.."]\n🔱 نتیجه اسپم: اخراج شدن!")
 		else
 			savelog(msg.to.id, name_log.." ["..msg.from.id.."] kicked for #spam")
-			send_large_msg(receiver , "Flooding is not allowed here\nName:"..name_log.."["..msg.from.id.."]\nStatus: User kicked")
+			send_large_msg(receiver , "🔱متأسفانه اسپم زدن در این گروه مجاز نمیباشد!🔱\nName:"..name_log.."["..msg.from.id.."]\n🔱 نتیجه : اخراج از گروه. 🔱")
 		end
 	  end
       -- incr it on redis
@@ -121,7 +120,7 @@ local function pre_process(msg)
       local gbanspamonredis = redis:get(gbanspam)
       --Check if user has spammed is group more than 4 times  
       if gbanspamonredis then
-        if tonumber(gbanspamonredis) ==  4 and not is_owner(msg) then
+        if tonumber(gbanspamonredis) ==  7 and not is_owner(msg) then
           --Global ban that user
           banall_user(msg.from.id)
           local gbanspam = 'gban:spam'..msg.from.id
@@ -135,13 +134,13 @@ local function pre_process(msg)
           local print_name = user_print_name(msg.from):gsub("‮", "")
 		  local name = print_name:gsub("_", "")
           --Send this to that chat
-          send_large_msg("chat#id"..msg.to.id, "User [ "..name.." ]"..msg.from.id.." globally banned (spamming)")
-		  send_large_msg("channel#id"..msg.to.id, "User [ "..name.." ]"..msg.from.id.." globally banned (spamming)")
+          send_large_msg("chat#id"..msg.to.id, "🔱 متأسفانه کاربر: [ "..name.." ]"..msg.from.id.." بصورت دائمی و جهانی بدلیل اسپم های مکرر از گروهای ربات تله گولد برای همیشه اخراج شد. 🔱")
+		  send_large_msg("channel#id"..msg.to.id, "🔱 متأسفانه کاربر: [ "..name.." ]"..msg.from.id.." بصورت دائمی و جهانی بدلیل اسپم های مکرر از گروهای ربات تله گولد برای همیشه اخراج شد. 🔱")
           local GBan_log = 'GBan_log'
 		  local GBan_log =  data[tostring(GBan_log)]
 		  for k,v in pairs(GBan_log) do
 			log_SuperGroup = v
-			gban_text = "User [ "..name.." ] ( @"..username.." )"..msg.from.id.." Globally banned from ( "..msg.to.print_name.." ) [ "..msg.to.id.." ] (spamming)"
+			gban_text = "🔱 کاربر: [ "..name.." ] ( @"..username.." )"..msg.from.id.." بصورت جهانی و دائمی از گروهای ربات تله گولد خارج شد بدلیل اسپم های مکرر!!! 🔱 ( "..msg.to.print_name.." ) [ "..msg.to.id.." ] 🔱"
 			--send it to log group/channel
 			send_large_msg(log_SuperGroup, gban_text)
 		  end
